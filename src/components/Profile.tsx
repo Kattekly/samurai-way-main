ee817e85f9c1146f4b58c3e30bacaeadf0d19f61
Ekaterina <kattekly@mail.ru>
2022-09-13 13:37:35 +0300

    Change lessons 11, 12

---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://img2.fonwall.ru/o/gw/gory-zakat-solnce.jpg?route=thumb&h=350"/>
+            </div>
+            <div>Ava + Dis</div>
+            <div>My Posts
+                <div>New posts</div>
+            </div>
+            <div>
+                <div>
+                    post1
+                </div>
+                <div>
+                    post2
+                </div>
+            </div>
+
+        </div>
+    );
+};
+
+export default Profile;
