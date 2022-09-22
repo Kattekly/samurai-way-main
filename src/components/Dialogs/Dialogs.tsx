bd6155fa1004beaa6fb9e12fd5cc5350cf994b10
Ekaterina <kattekly@mail.ru>
2022-09-22 11:55:06 +0300

    Lessons-21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -5,7 +5,7 @@ const Dialogs = () => {
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>
+                <div className={s.dialog + ' ' + s.active}>
                     Katerina
                 </div>
                 <div className={s.dialog}>
