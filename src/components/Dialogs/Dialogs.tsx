3d3f3c11304260d2de431911bda9391eaa203529
Ekaterina <kattekly@mail.ru>
2022-09-22 11:46:45 +0300

    Lessons-21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,32 @@ import s from './Dialogs.module.css'
 
 const Dialogs = () => {
     return (
-        <div className={s.content}>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog}>
+                    Katerina
+                </div>
+                <div className={s.dialog}>
+                    Vladimir
+                </div>
+                <div className={s.dialog}>
+                    Andrey
+                </div>
+                <div className={s.dialog}>
+                    Valera
+                </div>
+                <div className={s.dialog}>
+                    Maksim
+                </div>
+                <div className={s.dialog}>
+                    Maria
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How are you?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 };
