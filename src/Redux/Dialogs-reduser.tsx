56789522b2dea46370a25daf98a964144c77db26
Ekaterina <kattekly@mail.ru>
2022-10-21 10:45:35 +0300

    Lesson-41

--- src/Redux/Dialogs-reduser.tsx
+++ src/Redux/Dialogs-reduser.tsx
@@ -5,18 +5,19 @@ const NEW_MESSAGE_TEXT = 'NEW-MESSAGE-TEXT'
 
 export const dialogsReducer = (state:  RootStateType, action: ActionTypes) => {
 
-if (action.type === SEND_MESSAGE) {
-        let body = {
-            id: new Date().getTime(),
-            message: state.profilePage.newMessageText
-        }
-        state.profilePage.messages.push(body)
-        state.profilePage.newMessageText = ''
-
-    }else if (action.type === NEW_MESSAGE_TEXT) {
-        state.profilePage.newMessageText = action.body
-
+    switch (action.type) {
+        case SEND_MESSAGE:
+            let body = {
+                id: new Date().getTime(),
+                message: state.profilePage.newMessageText
+            }
+            state.profilePage.messages.push(body)
+            state.profilePage.newMessageText = ''
+            return state
+        case NEW_MESSAGE_TEXT:
+            state.profilePage.newMessageText = action.body
+            return state
+        default:
+            return state
     }
-
-    return state
 }
