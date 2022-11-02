492a56a23df8d89b720f84f818d12b717a244f4a
Ekaterina <kattekly@mail.ru>
2022-11-02 13:24:35 +0300

    Lesson-49

--- src/App.tsx
+++ src/App.tsx
@@ -41,6 +41,7 @@ const App = (props: AppPropsType) => {
                 <Route path="/news" component={News}/>
                 <Route path="/music" component={Music}/>
                 <Route path="/settings" component={Settings}/>
+                <Route path="/users" render={() => <div></div>}/>
             </div>
         </div>
     );
