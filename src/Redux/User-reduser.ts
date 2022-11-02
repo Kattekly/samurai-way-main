30ee5feb934b9dcf29d7de825081886f7e73fd5d
Ekaterina <kattekly@mail.ru>
2022-11-02 14:30:20 +0300

    Lesson-49

--- src/Redux/User-reduser.ts
+++ src/Redux/User-reduser.ts
@@ -1,3 +1,6 @@
+const FOLLOW = "FOLLOW"
+const UNFOLLOW = "UNFOLLOW"
+
 
 type UsersMaxPropsType = {
     users: Array<UsersPropsType>
@@ -5,6 +8,7 @@ type UsersMaxPropsType = {
 
 type UsersPropsType = {
     id: number
+    followed: boolean
     fullName: string
     status: string
     location: LocationPropsType
@@ -17,16 +21,58 @@ type LocationPropsType = {
 
 let initialState: UsersMaxPropsType = {
     users: [
-        {id: 1, fullName: "Katerina", status: "I am a boss", location: {city: "Rybinsk", country: "Russia"}},
-        {id: 2, fullName: "Vladimir", status: "Hey, hoy!", location: {city: "Moscow", country: "Russia"}},
-        {id: 3, fullName: "Marina", status: "Sun", location: {city: "Minsk", country: "Belarus"}},
+        {
+            id: 1,
+            followed: false,
+            fullName: "Katerina",
+            status: "I am a boss",
+            location: {city: "Rybinsk", country: "Russia"}
+        },
+        {
+            id: 2,
+            followed: true,
+            fullName: "Vladimir",
+            status: "Hey, hoy!",
+            location: {city: "Moscow", country: "Russia"}
+        },
+        {
+            id: 3,
+            followed: false,
+            fullName: "Marina",
+            status: "Sun",
+            location: {city: "Minsk", country: "Belarus"}
+        },
     ]
 }
 
-const UsersReduser = (state: any, action: any) => {
-switch (action.type) {
-    default: return state
-}
+const UsersReduser = (state = initialState, action: any) => {
+    switch (action.type) {
+        case FOLLOW:
+            return {
+                ...state,
+                users: state.users.map(el => {
+                    if (el.id === action.userId) {
+                        return {...el, followed: true}
+                    }
+                 return el
+                })
+            }
+        case UNFOLLOW:
+            return {
+                ...state,
+                users: state.users.map(el => {
+                    if (el.id === action.userId) {
+                        return {...el, followed: false}
+                    }
+                    return el
+                })
+            }
+        default:
+            return state
+    }
 };
+
+export const followAC = (userId: number) => ({type: FOLLOW, userId})
+export const unfollowAC = (userId: number) => ({type: UNFOLLOW, userId})
 
 export default UsersReduser;
