bd29ee849f8d7541aec5cad534295cda5430ddbe
Ekaterina <kattekly@mail.ru>
2022-11-24 16:03:39 +0300

    Lesson-63

--- src/api/Api.tsx
+++ src/api/Api.tsx
@@ -1,32 +1,27 @@
 import axios from "axios";
 
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": "cef20686-9f7c-4e70-b5ff-96c74b679419"
+    }
+})
+
+
 export const getUsers = (currentPage: number, pageSize: number) => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${currentPage}&count=${pageSize}`,
-        {
-            withCredentials: true
-        }).then(response => response.data)
+    return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+        .then(response => response.data)
 }
 
 export const deleteUnfollow = (id: number) => {
-    return axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "cef20686-9f7c-4e70-b5ff-96c74b679419"
-        }
-    }).then(response => response.data)
+    return instance.delete(`follow/${id}`, ).then(response => response.data)
 }
 
 export const postFollow = (id: number) => {
-    return axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${id}`, {}, {
-        withCredentials: true,
-        headers: {
-            "API-KEY": "cef20686-9f7c-4e70-b5ff-96c74b679419"
-        }
-    }).then(response => response.data)
+    return instance.post(`follow/${id}`, {}).then(response => response.data)
 }
 
 export const getHeader = () => {
-    return axios.get(`https://social-network.samuraijs.com/api/1.0/auth/me`, {
-        withCredentials: true
-    }).then(response => response.data)
+    return instance.get(`auth/me`).then(response => response.data)
 }
