47bbe8524bcfd00c909bf9f51730808666d66b00
Ekaterina <kattekly@mail.ru>
2022-11-24 09:42:18 +0300

    Lesson-61

---
+++ src/Redux/AuthReduser.ts
@@ -0,0 +1,25 @@
+
+
+type initialStatePropsType = {
+    id: number
+    email: string
+    login: string
+    isFetching: boolean
+}
+
+let initialState: initialStatePropsType = {
+    id: 2,
+    email: 'blabla@bla.bla',
+    login: '',
+    isFetching: true
+}
+
+import React from 'react';
+
+const AuthReduser = () => {
+    return {
+
+    }
+};
+
+export default AuthReduser;
