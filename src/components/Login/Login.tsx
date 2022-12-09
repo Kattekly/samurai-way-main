fc46da252aac89270df562edc88538ea7373e516
Ekaterina <kattekly@mail.ru>
2022-12-09 11:10:36 +0300

    Lesson-75

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import {reduxForm} from "redux-form";
+import {Field, reduxForm} from "redux-form";
 
-export const LoginForm = () => {
+export const LoginForm = (props: any) => {
     return (
-            <form>
-                <div>
-                    <input placeholder={"Login"}/>
-                </div>
-                <div>
-                    <input placeholder={"Password"}/>
-                </div>
-                <div>
-                    <input type={"checkbox"}/> remember me
-                </div>
-                <div>
-                    <button>Login</button>
-                </div>
-            </form>
+        <form onSubmit={props.handleSubmit}>
+            <div>
+                <Field placeholder={"Login"} name={"login"} component={"input"}/>
+            </div>
+            <div>
+                <Field placeholder={"Password"} name={"password"} component={"input"}/>
+            </div>
+            <div>
+                <Field component={"input"} name={"rememberMe"} type={"checkbox"}/> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
     );
 };
 
-const ReduxLoginForm = reduxForm({form: 'login'})(LoginForm)
+export const ReduxLoginForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = () => {
- return  <div>
+    return <div>
         <h1>Login</h1>
-        <LoginForm/>
+        <ReduxLoginForm/>
     </div>
 }
 
