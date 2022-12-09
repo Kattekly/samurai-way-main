8be9c6f206d9c68b717350611f0694066ba6ad91
Ekaterina <kattekly@mail.ru>
2022-12-09 10:36:50 +0300

    Lesson-75

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -5,7 +5,18 @@ const Login = () => {
         <div>
             <h1>Login</h1>
             <form>
-
+                <div>
+                    <input placeholder={"Login"}/>
+                </div>
+                <div>
+                    <input placeholder={"Password"}/>
+                </div>
+                <div>
+                    <input type={"checkbox"}/> remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
             </form>
         </div>
     );
