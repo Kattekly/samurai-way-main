77fd4d6a0603e38679f80b8f3013cd0e38bda488
Ekaterina <kattekly@mail.ru>
2022-12-23 11:45:17 +0300

    Lesson-77

--- src/components/Profile/My posts/MyPosts.tsx
+++ src/components/Profile/My posts/MyPosts.tsx
@@ -3,7 +3,8 @@ import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 import {MessageType} from "../../../App";
 import {Field, InjectedFormProps, reduxForm} from "redux-form";
-import {required} from "../../../utils/validators/validators";
+import {maxLengthCreator, required} from "../../../utils/validators/validators";
+import {Textarea} from "../../common/FormControls/FormsControls";
 
 
 const MyPosts = (props: MessageType) => {
@@ -42,11 +43,14 @@ type FormDataType = {
     newPostText: string
 }
 
+
+const maxLength10 = maxLengthCreator(10)
+
 const AddNewPostForm: React.FC<InjectedFormProps<FormDataType>> = ({handleSubmit}) => {
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <Field component="textarea" name="newPostText" validate={[required]} />
+                <Field component={Textarea} name="newPostText" validate={[required, maxLength10]} />
             </div>
             <div>
                 <button>Add post</button>
