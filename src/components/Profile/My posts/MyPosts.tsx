e0d5aa39e24613d117a5b5cd0aeec2436153c04e
Ekaterina <kattekly@mail.ru>
2022-12-23 10:54:00 +0300

    Lesson-76

--- src/components/Profile/My posts/MyPosts.tsx
+++ src/components/Profile/My posts/MyPosts.tsx
@@ -1,20 +1,19 @@
-import React, {LegacyRef} from 'react';
+import React from 'react';
 import s from "./MyPosts.module.css"
 import Post from "./Post/Post";
 import {MessageType} from "../../../App";
-import {InjectedFormProps, reduxForm} from "redux-form";
+import {Field, InjectedFormProps, reduxForm} from "redux-form";
 
 
 const MyPosts = (props: MessageType) => {
     console.log({props})
     let postElement = props.posts.map(p => <Post key={p.id} message={p.message} like={p.like}/>)
 
-    let newPostElement = React.createRef <HTMLTextAreaElement>()
+    // let newPostElement = React.createRef <HTMLTextAreaElement>()
 
     let onAddPost = (values: any) => {
-
         if (props.addPost) {
-            props.addPost(props.newPostText)
+            props.addPost(values.newPostText)
         }
     }
 
@@ -46,15 +45,15 @@ const AddNewPostForm: React.FC<InjectedFormProps<FormDataType>> = ({handleSubmit
     return (
         <form onSubmit={handleSubmit}>
             <div>
-                <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
+                <Field name="newPostText"/>
             </div>
             <div>
-                <button onClick={onAddPost}>Add post</button>
+                <button>Add post</button>
             </div>
         </form>
     )
 }
 
-const AddNewPostFormRedux = reduxForm({form: "ProfileAddNewPostForm"})(AddNewPostForm)
+const AddNewPostFormRedux = reduxForm<FormDataType>({form: "ProfileAddNewPostForm"})(AddNewPostForm)
 
 export default MyPosts;
