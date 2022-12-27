58797b99b984012da527222b05a0a8c33c781d74
Ekaterina <kattekly@mail.ru>
2022-12-27 13:54:18 +0300

    Lesson-80

--- src/Redux/app-reducer.ts
+++ src/Redux/app-reducer.ts
@@ -1,3 +1,9 @@
+import {Dispatch} from "redux";
+import {authAPI} from "../api/Api";
+import {setAuthUserData} from "./AuthReducer";
+
+const SET_INITIALIZED = 'SET_INITIALIZED'
+
 type InitialStateType = {
     initialized: boolean
 }
@@ -6,6 +12,19 @@ let InitialState: InitialStateType = {
     initialized: false
 }
 
-export const AppReducer = (state: InitialState, action) => {
-    switch ()
+export const AppReducer = (state = InitialState, action: any) => {
+    switch (action.type) {
+        case SET_INITIALIZED:
+            return {
+                ...state,
+                initialized: true
+            }
+        default:
+            return state
+    }
+}
+
+export const initializedSuccess = () => ({type: SET_INITIALIZED})
+export const initialize = () => (dispatch: Dispatch) => {
+
 }
