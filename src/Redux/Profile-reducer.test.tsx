439ee9b4a213356be17ed58c977024c64c1bec4d
Ekaterina <kattekly@mail.ru>
2023-02-01 12:50:18 +0300

    Lesson-89

--- src/Redux/Profile-reducer.test.tsx
+++ src/Redux/Profile-reducer.test.tsx
@@ -1,6 +1,18 @@
-import {profileReducer} from "./Profile-reducer";
+import {addPostActionCreator, profileReducer} from "./Profile-reducer";
+import {type} from "os";
 
 
-test ('new post should be added', () => {
-    let newState = profileReducer({}, {})
+test('new post should be added', () => {
+    let initialState = {
+        posts: [
+            {id: 1, message: "Hi, how are yo?", like: 15},
+            {id: 2, message: "It's my first post", like: 20}
+        ],
+        profile: null,
+        status: ''
+    }
+    let action = addPostActionCreator('ananas')
+
+
+    let newState = profileReducer(initialState, action)
 })
