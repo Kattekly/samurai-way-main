2af41c243a598c56f781f9f92544e88bead4bd61
Ekaterina <kattekly@mail.ru>
2023-02-10 10:24:16 +0300

    Lesson-92

--- src/components/Profile/My posts/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/My posts/ProfileInfo/ProfileStatus.test.tsx
@@ -4,20 +4,30 @@ import ProfileStatus from "./ProfileStatus";
 
 describe("ProfileStatus", () => {
     test("Status from props should be in the state ", () => {
-        const component = create(<ProfileStatus status="Alalalol"/>);
+        const component = create(<ProfileStatus status="Alalalol" updateStatusThunk={() => {
+        }}/>);
         const instance = component.getInstance();
         expect(instance.state.status).toBe("Alalalol");
     });
     test("After creating span be correct", () => {
-        const component = create(<ProfileStatus status="Alalalol"/>);
+        const component = create(<ProfileStatus status="Alalalol" updateStatusThunk={() => {
+        }}/>);
         const root = component.root;
         let span = root.findByType("span")
-        expect(span.length).toBe(1);
+        expect(span).not.toBeNull();
     });
     test("Status = text", () => {
-        const component = create(<ProfileStatus status="Alalalol"/>);
+        const component = create(<ProfileStatus status="Alalalol" updateStatusThunk={() => {
+        }}/>);
         const root = component.root;
         let span = root.findByType("span")
         expect(span.innerText).toBe("Alalalol");
+    });
+    test("Inputt null", () => {
+        const component = create(<ProfileStatus status="Alalalol" updateStatusThunk={() => {
+        }}/>);
+        const root = component.root;
+        let span = root.findByType("input")
+        expect(span).not.toBeNull();
     });
 });
