3e190121eedc0494bfcc6e6c72f8ba2b41ae5fbf
Ekaterina <kattekly@mail.ru>
2023-02-10 10:35:32 +0300

    Lesson-92

--- src/components/Profile/My posts/ProfileInfo/ProfileStatus.test.tsx
+++ src/components/Profile/My posts/ProfileInfo/ProfileStatus.test.tsx
@@ -32,4 +32,13 @@ describe("ProfileStatus", () => {
             let input = root.findByType("input")
         }).toThrow();
     });
+    test("input should be displayed in editMode instead of span", () => {
+        const component = create(<ProfileStatus status="Alalalol" updateStatusThunk={() => {
+        }}/>);
+        const root = component.root;
+        let span = root.findByType("span")
+        span.props.onDoubleClick()
+        let input = root.findByType("input")
+        expect(input.props.value).toBe("Alalalol");
+    });
 })
