19568ab890a1bcb9b47c03851c012c0f4d937872
Ekaterina <kattekly@mail.ru>
2023-02-24 13:07:54 +0300

    Lesson-94

--- src/index.tsx
+++ src/index.tsx
@@ -3,15 +3,15 @@ import './index.css';
 import store from "./Redux/Redux-Stor";
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
 
 ReactDOM.render(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>,
+    </HashRouter>,
     document.getElementById('root')
 );
