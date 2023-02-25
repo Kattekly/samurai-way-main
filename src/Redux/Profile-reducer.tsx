2ba0f3b3272d982d8ba2798e2b8ce6b9460c9c3d
Ekaterina <kattekly@mail.ru>
2023-02-25 22:18:57 +0300

    Lesson-96

--- src/Redux/Profile-reducer.tsx
+++ src/Redux/Profile-reducer.tsx
@@ -2,7 +2,6 @@ import {ProfileAPI} from "../api/Api";
 import profile from "../components/Profile/Profile";
 
 const ADD_POST = 'ADD-POST'
-// const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT'
 const SET_USER_PROFILE = 'SET_USER_PROFILE'
 const SET_STATUS = 'SET_STATUS'
 const DELETE_POST = 'DELETE_POST'
