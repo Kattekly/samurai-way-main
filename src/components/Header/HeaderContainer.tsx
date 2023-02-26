c824061ad38a4c83d7440e99dd57ded5d3d51457
Ekaterina <kattekly@mail.ru>
2023-02-26 17:31:42 +0300

    Lesson-96

--- src/components/Header/HeaderContainer.tsx
+++ src/components/Header/HeaderContainer.tsx
@@ -5,15 +5,6 @@ import {initialStatePropsType, LogOut} from "../../Redux/AuthReducer";
 import {ReduxStateType} from "../../Redux/Redux-Stor";
 
 class HeaderContainer extends React.Component<any, any> {
-   /* componentDidMount() {
-        this.props.getLogin()*/
-
-        /* authAPI.me().then(data => {
-             if (data.resultCode === 0) {
-                 let {id, email, login} = data.data
-                 this.props.setAuthUserData(id, email, login);
-             }
-         })*/
 
     render() {
         return <Header isAuth={this.props.isAuth} login={this.props.login} logOut={LogOut}/>
