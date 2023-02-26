0d60ae5f0561967e1403f3d90c6334269e82afd0
Ekaterina <kattekly@mail.ru>
2023-02-26 17:31:14 +0300

    Lesson-96

--- src/components/Profile/My posts/Post/Post.tsx
+++ src/components/Profile/My posts/Post/Post.tsx
@@ -7,7 +7,7 @@ const Post = (props: PostType) => {
 
     return (
         <div className={s.item}>
-            <img src="https://cdn1.flamp.ru/de5d137bb9fffdca9df095726d4100e6.jpeg"/>
+            {/*<img src="https://cdn1.flamp.ru/de5d137bb9fffdca9df095726d4100e6.jpeg"/>*/}
             {props.message}
             <div>
                 <span>Like</span>
