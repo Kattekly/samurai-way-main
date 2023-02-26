27a2338d6aae2f5f729189d07ac5309ddcc00d43
Ekaterina <kattekly@mail.ru>
2023-02-26 17:33:08 +0300

    Lesson-96

--- src/components/Dialogs/DialogsContainer.tsx
+++ src/components/Dialogs/DialogsContainer.tsx
@@ -8,29 +8,8 @@ import {ReduxStateType} from "../../Redux/Redux-Stor";
 import withAuthRedirect from "../../hoc/AuthRedirect";
 import {compose} from "redux";
 
-/*type DialogNewType = {
-    store: Store<ReduxStateType, ActionTypes>
-}*/
-/*const DialogsContainer = (props: DialogNewType) => {
-    let state = props.store.getState().profilePage
-
-    let onSendMessageClick = () => {
-        props.store.dispatch(sendMessageCreator())
-    }
-
-    let onNewMessageChange = (body: string) => {
-        props.store.dispatch(updateMessageBodyCreator(body))
-    }
-
-    return (
-        <Dialogs updateMessageBodyCreator={onNewMessageChange}
-                 sendMessageCreator={onSendMessageClick} profilePage={state}/>
-    );
-};*/
-
 //данные из стейна, пропсы
 let mapStateToProps = (state: ReduxStateType) => {
-
     return {
         profilePage: state.profilePage,
     }
@@ -39,19 +18,11 @@ let mapStateToProps = (state: ReduxStateType) => {
 //колбеки
 let mapDispatchToProps = (dispatch: (action: ActionTypes) => void) => {
     return {
-       /* updateMessageBodyCreator: (body: string) => {
-            dispatch(updateMessageBodyCreator(body))
-        },*/
         sendMessageCreator: (newMessageText: string) => {
             dispatch(sendMessageCreator(newMessageText))
         }
     }
 }
-
-
-/*let AuthRedirectComponent = withAuthRedirect(Dialogs)
-
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(AuthRedirectComponent)*/
 
 export default compose<React.ComponentType>(
     connect(mapStateToProps, mapDispatchToProps),
