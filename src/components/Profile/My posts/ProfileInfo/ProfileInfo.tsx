c1517d8337a772404c53ffbb707ca2407ff8baa8
Ekaterina <kattekly@mail.ru>
2023-02-27 15:33:08 +0300

    Lesson-97

--- src/components/Profile/My posts/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/My posts/ProfileInfo/ProfileInfo.tsx
@@ -82,23 +82,7 @@ const ProfileData: React.FC<NewType> = ({profile, isOwner, toEditMode}) => {
 const ProfileDataForm: React.FC<NewType> = ({profile}) => {
     return <>
         <div>
-            <b>Full name</b>: {profile.fullName}
-        </div>
-        <div>
-            <b>Looking for a job</b>: {profile.lookingForAJob ? 'yes' : 'no'}
-        </div>
-        {profile.lookingForAJob &&
-            <div>
-                <b>My professionals skills</b>: {profile.lookingForAJobDescription}
-            </div>
-        }
-        <div>
-            <b>About me</b>: {profile.aboutMe}
-        </div>
-        <div>
-            <b>Contacts</b>: {Object.keys(profile.contacts).map(key => {
-            return <Contact key={key} contactTitle={key} contactValue={profile.contacts[key]}/>
-        })}
+            Form
         </div>
     </>
 }
