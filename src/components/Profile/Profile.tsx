73421d6e4067c5afa122dc7e029b1b84413c856c
Ekaterina <kattekly@mail.ru>
2023-03-04 11:59:16 +0300

    styles

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -16,7 +16,10 @@ export type ProfileNewType = {
 const Profile = (props: ProfileNewType) => {
     return (
         <div>
-
+            <div >
+                <img className={s.headerImg} alt='big content'
+                     src='https://sun9-28.userapi.com/impf/IZV8ZqKT5iFUZ5RcKf17lskCX9W354PG0UFpWg/BogcuCe_SGc.jpg?size=1920x768&quality=95&crop=0,25,2560,1022&sign=0cc3f2d38c31cc82955bebdec766f149&type=cover_group'/>
+            </div>
             <ProfileInfo profile={props.profile} saveProfile={props.saveProfile} status={props.status}
                          updateStatusThunk={props.updateStatusThunk} isOwner={props.isOwner}
                          savePhoto={props.savePhoto}/>
