77176931efa1f640e6e901c62871dca4652cdb4c
Ekaterina <kattekly@mail.ru>
2023-03-05 15:47:01 +0300

    styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,27 +1,20 @@
 import React from 'react';
 import s from "./Navbar.module.css"
 import {NavLink} from "react-router-dom";
-import SvgIcon, {SvgIconProps} from '@material-ui/core/SvgIcon';
+import HomeIcon from '@material-ui/icons/Home';
 import BuildIcon from '@material-ui/icons/Build';
 import HeadsetIcon from '@material-ui/icons/Headset';
 import PeopleIcon from '@material-ui/icons/People';
 import TextsmsIcon from '@material-ui/icons/Textsms';
 import WebAssetIcon from '@material-ui/icons/WebAsset';
 
-function HomeIcon(props: SvgIconProps) {
-    return (
-        <SvgIcon {...props}>
-            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
-        </SvgIcon>
-    );
-}
 
 console.log(s)
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <HomeIcon color="primary" className={s.icon}/>
+                <HomeIcon color="primary" fontSize="small"/>
                 <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
@@ -43,6 +36,11 @@ const Navbar = () => {
             <div className={s.item}>
                 <BuildIcon color="primary" fontSize="small"/>
                 <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
+            </div>
+            <div className={s.reclama}>
+                Place for your advertisement:
+                <img  alt='big content'
+                     src='https://i.pinimg.com/originals/8c/f2/c2/8cf2c2b62722f1971a7aad52fc597d22.jpg'/>
             </div>
         </nav>
     );
