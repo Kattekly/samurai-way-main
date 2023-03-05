de0f5ecf4a22e48aa8971928d2522c658446a40a
Ekaterina <kattekly@mail.ru>
2023-03-05 15:16:03 +0300

    styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import s from "./Navbar.module.css"
 import {NavLink} from "react-router-dom";
+import SvgIcon, {SvgIconProps} from '@material-ui/core/SvgIcon';
+import BuildIcon from '@material-ui/icons/Build';
+
+function HomeIcon(props: SvgIconProps) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        </SvgIcon>
+    );
+}
 
 console.log(s)
 const Navbar = () => {
     return (
         <nav className={s.nav}>
             <div className={s.item}>
+                <HomeIcon color="primary" className={s.icon}/>
                 <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
             </div>
             <div className={`${s.item} ${s.active}`}>
@@ -22,6 +33,7 @@ const Navbar = () => {
                 <NavLink to="/music" activeClassName={s.active}>Music</NavLink>
             </div>
             <div className={s.item}>
+                <BuildIcon color="primary" fontSize="small"/>
                 <NavLink to="/settings" activeClassName={s.active}>Settings</NavLink>
             </div>
         </nav>
