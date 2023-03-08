c2060d70a7eb9eec8940365b81d6965de1ac0fb3
Ekaterina <kattekly@mail.ru>
2023-03-08 20:19:04 +0300

    styles

--- src/Redux/dialogs-reduser.ts
+++ src/Redux/dialogs-reduser.ts
@@ -4,16 +4,16 @@ const SEND_MESSAGE = 'SEND-MESSAGE'
 
 let initialState = {
     dialogs: [
-        {id: 1, name: "Katerina", avatar: 'https://cdn-icons-png.flaticon.com/512/194/194938.png'},
-        {id: 2, name: "Vladimir", avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector.png'},
-        {id: 3, name: "Andrey", avatar: "https://img2.freepng.ru/20180904/vji/kisspng-avatar-image-computer-icons-likengo-usertesting-index-5b8ec1242fdcf5.6000571015360822121961.jpg"},
-        {id: 4, name: "Valera"},
-        {id: 5, name: "Maksim"},
+        {id: 1, name: "Katerina", avatar: 'https://kartinkin.net/uploads/posts/2022-03/thumbs/1647955342_64-kartinkin-net-p-kartinki-kotikov-risunki-67.jpg'},
+        {id: 2, name: "Vladimir", avatar: 'https://i.pinimg.com/736x/b2/6c/45/b26c451145cb780d96ac7a7ec90d541b.jpg'},
+        {id: 3, name: "Andrey", avatar: "https://i.pinimg.com/originals/82/72/b0/8272b07b71e159c9069e0dd7756bd9be.jpg"},
+        {id: 4, name: "Valera", avatar: "https://i.pinimg.com/originals/73/ec/31/73ec31a41dc09194ab79c6327f4cde19.jpg"},
+        {id: 5, name: "Maksim", avatar: "https://i.pinimg.com/originals/14/2c/8c/142c8c170beab057c71c3b6a8fb587b5.jpg"},
     ],
     messages: [
-        {id: 1, message: "Hi", avatar: "https://img2.freepng.ru/20180904/vji/kisspng-avatar-image-computer-icons-likengo-usertesting-index-5b8ec1242fdcf5.6000571015360822121961.jpg"},
-        {id: 2, message: "How are you?", avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector.png'},
-        {id: 3, message: "Yo", avatar: 'https://www.pngall.com/wp-content/uploads/12/Avatar-Profile-Vector.png'},
+        {id: 1, message: "Hi", avatar: "https://i.pinimg.com/736x/b2/6c/45/b26c451145cb780d96ac7a7ec90d541b.jpg"},
+        {id: 2, message: "How are you?", avatar: 'https://i.pinimg.com/736x/b2/6c/45/b26c451145cb780d96ac7a7ec90d541b.jpg'},
+        {id: 3, message: "Yo", avatar: 'https://i.pinimg.com/736x/b2/6c/45/b26c451145cb780d96ac7a7ec90d541b.jpg'},
     ],
 }
 
